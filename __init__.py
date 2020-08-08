7cef700f15b08545c737322ca1db4732fbe2bd94
Glenn Streetman <glennstreetman@gmail.com>
2020-08-08 10:45:45 -0700

    updated requirements, removed flaskr references

--- __init__.py
+++ __init__.py
@@ -24,9 +24,9 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    from flaskr import db
+    # from flaskr import db
 
-    db.init_app(app)
+    # db.init_app(app)
 
     from pictureApp import pictures
     app.register_blueprint(pictures.bp)
